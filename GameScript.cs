f06498e5132bdf9c5ae9a9253776f5ff2a19d663
April <April@Aprils-MacBook-Air.local>
2014-12-08 07:05:46 -0800

    add scenes and limitations

---
+++ GameScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour {
+
+	public float timeLeft;
+	Text txt;
+
+
+	// Use this for initialization
+	void Start () {
+		txt = gameObject.GetComponent<Text> ();
+		timeLeft = 300.0f;
+		txt.text = "Time:" + timeLeft;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		txt.text = "Time:" + (int)timeLeft;
+		if (timeLeft < 0) {
+			GameOver();
+		}
+	}
+
+	void GameOver(){
+		Application.LoadLevel("lose");
+	}
+}
